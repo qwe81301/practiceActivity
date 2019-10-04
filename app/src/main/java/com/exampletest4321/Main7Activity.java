6ba6650cdc030c9d6fe366a974425cdd6fefea56
shih-jin-yu <qwe81301@gmail.com>
2019-10-04 09:39:53 +0800

    webview 實做

---
+++ app/src/main/java/com/exampletest4321/Main7Activity.java
@@ -0,0 +1,27 @@
+package com.exampletest4321;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class Main7Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main7);
+
+        WebView webview = (WebView) findViewById(R.id.webView);
+        WebSettings webSettings = webview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webview.setWebViewClient(new WebViewClient());
+//        webview.loadUrl("https://jerrynest.io/");
+        String web1= "http://192.168.1.195/lightBox/ResumeDetail.aspx?Row=aa997479-14d2-4047-96b0-c02dbc812a83&Snap=94c82160-5a72-46cf-9a9f-ac03a237e3f2&role=1&tNo=52407601&rNo=3000999&ref=rPoolActive&vrS=1&eNo=85849114";
+        String web2= "http://192.168.1.234/17SP/6263test/20190606_resume-preview/resume-preview.htm";
+        webview.loadUrl(web1);
+
+    }
+}
