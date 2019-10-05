40252d566502173e7edd434f52b2ecf6af0ca43a
shih-jin-yu <qwe81301@gmail.com>
2019-10-05 17:08:09 +0800

    (EX 2 案例)某些狀況需要把 寬(android:layout_width)或高(android:layout_height)的單位設定為0dp (不對元件直接設定長或寬 而是用其他參數設定約束長或寬比例) EX 2: android:layout_height="0dp" app:layout_constraintHeight_percent="0.5"

---
+++ app/src/main/java/com/exampletest4321/Main8Activity.java
@@ -0,0 +1,25 @@
+package com.exampletest4321;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class Main8Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main8);
+
+        WebView webview = (WebView) findViewById(R.id.webView);
+        WebSettings webSettings = webview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webview.setWebViewClient(new WebViewClient());
+//        webview.loadUrl("https://jerrynest.io/");
+        String web1= "http://192.168.1.195/lightBox/ResumeDetail.aspx?Row=aa997479-14d2-4047-96b0-c02dbc812a83&Snap=94c82160-5a72-46cf-9a9f-ac03a237e3f2&role=1&tNo=52407601&rNo=3000999&ref=rPoolActive&vrS=1&eNo=85849114";
+        webview.loadUrl(web1);
+    }
+}
